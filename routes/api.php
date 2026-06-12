b84271525f8ea7291283f98a4546f417a0401bf3
zidan aliansyah shidiq <zidanaliansyah11@gmail.com>
2026-06-13 00:07:45 +0700

    tugas 3

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DoctorScheduleController;
 
-Route::prefix('v1')->middleware('api.key')->group(function () {
+Route::prefix('v1')->group(function () {
     Route::get('/doctor-schedules', [DoctorScheduleController::class, 'index']);
     Route::get('/doctor-schedules/{id}', [DoctorScheduleController::class, 'show']);
     Route::post('/doctor-schedules', [DoctorScheduleController::class, 'store']);
